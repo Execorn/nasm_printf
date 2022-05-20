c63c04d01e8e6dcc36010fd6c45c7befee6fa05d
Execorn <n.biryukov.04@gmail.com>
2022-05-20 20:27:37 +0300

    example changed

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 extern void my_printf();
 
 int main () {
-    my_printf("Hello %s! Python is %d times slower than C.\n%d %s %x %d%%%c%b\n", "world", 20, -1, "love", 3802, 100, '!', 127);
+    my_printf("Hello %s! Python is %d (oct = %o) times slower than C.\n%d %s %x %d%%%c%b\n", 
+                        "world", 20, 20, -1, "love", 3802, 100, '!', 127);
     return 0;
 }
